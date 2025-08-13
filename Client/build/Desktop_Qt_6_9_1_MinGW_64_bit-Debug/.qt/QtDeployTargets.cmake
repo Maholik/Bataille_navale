853d17c5d4054d1f2115361fd37f62acc8ae7580
Bilal Sallah <58287@etu.he2b.be>
2025-08-13 07:54:50 +0200

    retrait placewindow

--- Client/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Client_FILE D:/project/atlir5-bataillenavale/Client/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/Client.exe)
-set(__QT_DEPLOY_TARGET_Client_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Client_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
